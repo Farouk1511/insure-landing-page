d1ad194a957dc5b01f8760d482de22b71249b7ac
Farouk Kazeem <kazeem.farouk@yahoo.com>
2020-09-10 11:42:11 -0400

    finilized project - optimized for all devices

---
+++ script.js
@@ -0,0 +1,16 @@
+const burger = document.getElementById("burger");
+const mobileNav = document.getElementById("mobile-nav");
+let panel = "hamburger";
+burger.addEventListener("click", () => {
+  
+  if (panel === "hamburger") {
+    burger.src = "./images/icon-close.svg";
+    panel = "close";
+  } else if (panel === "close") {
+    burger.src = "./images/icon-hamburger.svg";
+    panel = "hamburger";
+  }
+
+  mobileNav.classList.toggle('show')
+  
+});
